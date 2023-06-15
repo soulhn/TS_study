5af1475f4af2a73ba9c2dcf60d7b7c6539af803e
soulhn <vldnrlf123@naver.com>
2023-06-15 14:40:30 +0900

    fix: 반복되는 부분 map함수로 수정

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,44 +6,41 @@ import Learn1 from "./components/Learn1";
 import Learn2 from "./components/Learn2";
 import Learn3 from "./components/Learn3";
 import Learn4 from "./components/Learn4";
+import Learn5 from "./components/Learn5";
+
 import EventCapturing from "./components/EventCapturing";
+
 function App() {
+  const learns = [
+    { path: "/learn1", component: <Learn1 />, name: "Learn1" },
+    { path: "/learn2", component: <Learn2 />, name: "Learn2" },
+    { path: "/learn3", component: <Learn3 />, name: "Learn3" },
+    { path: "/learn4", component: <Learn4 />, name: "Learn4" },
+    { path: "/learn5", component: <Learn5 />, name: "Learn5" },
+  ];
+
   return (
     <div className="App">
       <nav>
         <ul>
-          <li>
-            <Link to="/learn1">Learn1</Link>
-          </li>
-          <li>
-            <Link to="/learn2">Learn2</Link>
-          </li>
-          <li>
-            <Link to="/learn3">Learn3</Link>
-          </li>
-          <li>
-            <Link to="/learn4">Learn4</Link>
-          </li>
+          {learns.map((Learn, index) => (
+            <li key={index}>
+              <Link to={`/learn${index + 1}`}>Learn{index + 1}</Link>
+            </li>
+          ))}
           <li>
             <Link to="/eventBubbling">eventBubbling</Link>
           </li>
           <li>
             <Link to="/eventCapturing">EventCapturing</Link>
           </li>
-          {/* <li>
-            <Link to="/mypage">MyPage</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li> */}
         </ul>
       </nav>
 
       <Routes>
-        <Route path="/learn1" element={<Learn1 />} />
-        <Route path="/learn2" element={<Learn2 />} />
-        <Route path="/learn3" element={<Learn3 />} />
-        <Route path="/learn4" element={<Learn4 />} />
+        {learns.map((learn, index) => {
+          return <Route key={index} path={learn.path} element={learn.component} />;
+        })}
         <Route path="/eventBubbling" element={<EventBubbling />}></Route>
         <Route path="/eventCapturing" element={<EventCapturing />}></Route>
       </Routes>
---
+++ my-app/src/components/Learn5.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Learn5() {
+  return <div></div>;
+}
+
+export default Learn5;
